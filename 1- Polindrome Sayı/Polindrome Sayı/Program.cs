51399cc0ce7e5139b9dc7814aaa4c91ce7ccac53
Enes Kurt <eneskurt1996@hotmail.com>
2023-03-12 22:31:20 +0300

    Add project files.

---
+++ 1- Polindrome Sayı/Polindrome Sayı/Program.cs
@@ -0,0 +1,33 @@
+﻿int numberLength;
+int number;
+while (true)
+{
+    Console.Write("Kaç basamaklı sayı gireceğinizi giriniz:");
+    bool numberLengthIsInt = int.TryParse(Console.ReadLine(), out numberLength);
+    Console.Write("Test etmek istediğiniz sayıyı giriniz:");
+    bool numberIsInt = int.TryParse(Console.ReadLine(), out number);
+    string number1 = Convert.ToString(number);
+    if (numberLengthIsInt && numberIsInt && number > 0 && numberLength > 0 && numberLength == number1.Length)
+    {
+        break;
+    }
+    else Console.WriteLine("\nHatalı giriş yaptınız lütfen 0 dan büyük bir sayılar giriniz.");
+}
+
+int[] numbers = new int[numberLength];
+bool kontrol = true;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = number % 10;
+    number = number / 10;
+}
+
+for (int j = 0; j < numbers.Length; j++)
+{
+    if (!(numbers[j] == numbers[numbers.Length - j - 1]))        
+    kontrol = false;
+}
+
+string result = kontrol ? "\nSayi palindromdur." : "Sayi palindrom değildir.";
+Console.WriteLine(result);
