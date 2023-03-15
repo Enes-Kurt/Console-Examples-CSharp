95f0fc874711d9ec69cea86e175420590527c570
Enes Kurt <eneskurt1996@hotmail.com>
2023-03-15 21:45:28 +0300

    Soru 2 eklendi.

---
+++ 2- Sayıları Yazıyla Çevirme/Sayıları Yazıyla Çevirme/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("En fazla 5 basamaklı olacak şekilde yazıya çevirmek istediğiniz sayıyı giriniz: ");
+string tutar = Console.ReadLine();
+string yazi = "";
+char[] rakamlar = { '2', '3', '4', '5', '6', '7', '8', '9' };
+string[] birler = { "iki", "üç", "dört", "beş", "altı", "yedi", "sekiz", "dokuz" };
+string[] onlar = { "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan" };
+
+for (int i = 0; i < tutar.Length; i++)
+{
+
+    if (tutar[i] == '1')
+    {
+        if (i == tutar.Length - 5 && tutar.Length == 5) yazi += "on";
+        else if (i == tutar.Length - 4 && tutar.Length == 5) yazi += "bir";
+        if (i == tutar.Length - 4) yazi += "bin";
+        if (i == tutar.Length - 3) yazi += "yüz";
+        if (i == tutar.Length - 2) yazi += "on";
+        else if (i == tutar.Length - 1) yazi += "bir";
+    }
+    for (int j = 0; j < 8; j++)
+    {
+        if (tutar[i] == rakamlar[j])
+        {
+            if (i == tutar.Length - 5) yazi += onlar[j];
+            else if (i == tutar.Length - 2) yazi += onlar[j];
+            else yazi += birler[j];
+            if (i == tutar.Length - 4) yazi += "bin";
+            if (i == tutar.Length - 3) yazi += "yüz";
+        }
+    }
+}
+Console.WriteLine(yazi);
--- README.md
+++ README.md
@@ -4,7 +4,9 @@ C# yazýlým diliyle console uygulamasýnda yapýlan bazý algoritma örnekleri.
 
 ## 1- Polindrome Sayý
 
-Bir sayýnýn polindrome sayý olup olmadýðýný bulur.
+Bir sayýnýn polindrome sayý olup olmadýðýný bulur.  
+Örnek  
+Input: 32123  --> Output: "Sayý polindromdur."
 
 #### Programýn Çalýþmasý
 
@@ -12,3 +14,15 @@ Bir sayýnýn polindrome sayý olup olmadýðýný bulur.
 2. Kullanýcýdan kontrol edeceði sayýyý girmesini ister ve kullanýcýdan bu bilgiyi alýr.
 3. Sayýyý string olarak alýr ve array mantýðýyla sayýnýn kotrolünü yapar.
 4. Sayýnýn polindrome olup olmadýðýný ekrana yazdýrýr.
+
+## 2- Sayýlarý Yazýya Çevirme
+
+En fazla 5 basamaklý olacak þekilde yazýya çevirmek istediðiniz sayýyý yazý formatýnda yazar.  
+**Örnek**  
+Input: 54231  --> Output: ellidörtbinikiyüzotuzbir
+
+#### Programýn Çalýþmasý
+
+1. Kullanýcýdan çevirmek istediði sayýyý girmesini ister ve kullanýcýdan bu bilgiyi alýr.
+2. Sayýyý string olarak alýr ve basamaklara özel atanmýþ strin dizileri yardýmýyla boþ bir stringe sayýnýn yazý karþýlýðýný sýrayla ekler.
+3. Sayýnýn yazý olarak karþýlýðýný ekrana yazdýrýr.
